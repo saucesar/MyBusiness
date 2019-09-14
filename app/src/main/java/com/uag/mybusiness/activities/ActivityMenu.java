c455939b9f614deb2e86a3f34c37408ec715ff08
Eliaz500 <eliazzlima@gmail.com>
2019-09-14 11:26:08 -0300

    Botao login funcionando

---
+++ app/src/main/java/com/uag/mybusiness/activities/ActivityMenu.java
@@ -0,0 +1,16 @@
+package com.uag.mybusiness.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uag.mybusiness.R;
+
+public class ActivityMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
