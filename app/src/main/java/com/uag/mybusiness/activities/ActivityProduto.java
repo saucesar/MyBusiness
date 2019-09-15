e7abbb0f145157a6859957610a496067fd859cba
Eliaz500 <eliazzlima@gmail.com>
2019-09-15 02:17:09 -0300

    Activity produto criada

---
+++ app/src/main/java/com/uag/mybusiness/activities/ActivityProduto.java
@@ -0,0 +1,13 @@
+package com.uag.mybusiness.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityProduto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_produto);
+    }
+}
