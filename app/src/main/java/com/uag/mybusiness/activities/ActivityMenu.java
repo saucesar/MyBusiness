29f524f1a14d96c05b5b6fca510c0efedb24f034
Eliaz500 <eliazzlima@gmail.com>
2019-09-15 18:17:17 -0300

    Botao Produto funcionando

--- app/src/main/java/com/uag/mybusiness/activities/ActivityMenu.java
+++ app/src/main/java/com/uag/mybusiness/activities/ActivityMenu.java
@@ -2,15 +2,35 @@ package com.uag.mybusiness.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.uag.mybusiness.MainActivity;
 import com.uag.mybusiness.R;
 
 public class ActivityMenu extends AppCompatActivity {
+
+    private Button buttonProduto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+
+        this.buttonProduto = (Button) findViewById(R.id.buttonProduto);
+
+        this.buttonProduto.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //Chama a janela produto
+                Intent intent = new Intent(ActivityMenu.this, ActivityProduto.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
