d52b13d682210c0ab4f033fe799c5edfcb4f4044
saucesar <sau.cesarlima2013@gmail.com>
2019-09-16 21:24:20 -0300

    Adicao de getters em todos os atributos.

--- app/src/main/java/com/uag/mybusiness/entidades/Endereco.java
+++ app/src/main/java/com/uag/mybusiness/entidades/Endereco.java
@@ -14,10 +14,15 @@ public class Endereco {
 
     public String getCpfCliente(){return this.cpfCliente;}
     public void setCpfCliente(String cpfCliente){this.cpfCliente = cpfCliente;}
+    public String getRua(){ return this.rua; }
     public void setRua(String rua){this.rua = rua; }
+    public String getNumero(){ return this.numero; }
     public void setNumero(String numero){this.numero = numero; }
+    public String getBairro(){ return this.bairro; }
     public void setBairro(String bairro){this.bairro = bairro; }
+    public String getCidade(){ return this.cidade; }
     public void setCidade(String cidade){this.cidade = cidade; }
+    public String getEstado(){ return this.estado; }
     public void setEstado(String estado){this.estado = estado; }
 
     @Override
