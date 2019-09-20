5e07e3567fa682d1e0e7520a8e2169ebf9cb6c12
Eliaz500 <eliazzlima@gmail.com>
2019-09-20 00:30:25 -0300

    Tela de menu produto adicionada

---
+++ app/src/main/java/com/uag/mybusiness/activities/ActivityListaProdutos.java
@@ -0,0 +1,15 @@
+package com.uag.mybusiness.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.uag.mybusiness.R;
+
+public class ActivityListaProdutos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_produtos);
+    }
+}
