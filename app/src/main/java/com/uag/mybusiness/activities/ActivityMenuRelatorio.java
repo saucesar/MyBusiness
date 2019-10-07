e5805e3a2aaba3d763978339106b2bc2943bc0c8
Eliaz500 <eliazzlima@gmail.com>
2019-10-07 14:45:03 -0300

    Tela Usuario e Relatorio

---
+++ app/src/main/java/com/uag/mybusiness/activities/ActivityMenuRelatorio.java
@@ -0,0 +1,15 @@
+package com.uag.mybusiness.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.uag.mybusiness.R;
+
+public class ActivityMenuRelatorio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_relatorio);
+    }
+}
---
+++ app/src/main/java/com/uag/mybusiness/activities/ActivityMenuUsuario.java
@@ -0,0 +1,32 @@
+package com.uag.mybusiness.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.uag.mybusiness.R;
+
+public class ActivityMenuUsuario extends AppCompatActivity {
+
+    private Button buttonCadastrarUsuario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_admin);
+
+        this.buttonCadastrarUsuario = (Button) findViewById(R.id.buttonCadastrarUsuario);
+
+        this.buttonCadastrarUsuario.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //Chama a janela produto
+                Intent intent = new Intent(ActivityMenuUsuario.this, ActivityCadastrarUsuario.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
