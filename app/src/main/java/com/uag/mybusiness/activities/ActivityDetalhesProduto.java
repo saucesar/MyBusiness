b6e498f406e4fd528bd0073e7e45055dd9ae2490
Eliaz500 <eliazzlima@gmail.com>
2019-11-04 15:25:20 -0300

    Detalhes de Produto Imagem funcionando.

--- app/src/main/java/com/uag/mybusiness/activities/ActivityDetalhesProduto.java
+++ app/src/main/java/com/uag/mybusiness/activities/ActivityDetalhesProduto.java
@@ -2,12 +2,25 @@ package com.uag.mybusiness.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.uag.mybusiness.R;
 import com.uag.mybusiness.entidades.Produto;
+import com.uag.mybusiness.util.AdapterListaProdutos;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ActivityDetalhesProduto extends AppCompatActivity {
 
@@ -18,6 +31,8 @@ public class ActivityDetalhesProduto extends AppCompatActivity {
     private TextView precoProduto;
     private TextView quantidadeProduto;
     private TextView descricaoProduto;
+    private ImageButton buttonFotoAnterior;
+    private ImageButton buttonFotoProximo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +43,37 @@ public class ActivityDetalhesProduto extends AppCompatActivity {
         this.precoProduto = (TextView) findViewById(R.id.textViewDetalhesPrecoValor);
         this.quantidadeProduto = (TextView) findViewById(R.id.textViewDetalhesQuantidadeValor);
         this.descricaoProduto = (TextView) findViewById(R.id.textViewDetalhesDescricao);
+        this.fotoPrincipal = (ImageView) findViewById(R.id.imageViewDetalhesFoto);
+        this.buttonFotoAnterior = (ImageButton) findViewById(R.id.imageButtonAnteriorFoto);
+        this.buttonFotoProximo = (ImageButton) findViewById(R.id.imageButtonProximaFoto);
+
+        this.buttonFotoAnterior.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //Chama a janela produto
+
+               Log.d("Funcionou", ".......................................");
+
+
+            }
+        });
+
+        this.buttonFotoProximo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //Chama a janela produto
+
+                Log.d("Funcionou", ".......................................");
+                /*//------------Chama a funcao de ler url de imagem e joga essa imagem no XML
+                String URL1 = String.valueOf(produto.getFotoPrincipal());
+                ImageView imagem1 = (ImageView)findViewById(R.id.imageViewDetalhesFoto);
+                imagem1.setImageBitmap(DownloadImage(URL1));
+
+                this.fotoPrincipal.setImageBitmap(DownloadImage(URL1));*/
+
+
+            }
+        });
 
         Bundle detalhesProduto = getIntent().getExtras();
 
@@ -41,7 +87,7 @@ public class ActivityDetalhesProduto extends AppCompatActivity {
         produto.setFotoPrincipal(detalhesProduto.getString("foto_principal"));
         produto.setFotoSecundaria(detalhesProduto.getString("foto_secundaria"));
 
-        this.setEditTextEditarProduto(produto);
+        this.setTextViewDetalhesProduto(produto);
 
     }
 
@@ -49,11 +95,60 @@ public class ActivityDetalhesProduto extends AppCompatActivity {
     * Envia os valores para xlm
     *
     * */
-    private void setEditTextEditarProduto(Produto produto){
+   private void setTextViewDetalhesProduto(Produto produto){
         this.nomeProduto.setText(produto.getNome());
         this.precoProduto.setText(String.valueOf(produto.getPrecoVenda()));
         this.quantidadeProduto.setText(String.valueOf(produto.getQuantidade()));
         this.descricaoProduto.setText(produto.getDescricao());
+       //------------Chama a funcao de ler url de imagem e joga essa imagem no XML
+       String URL1 = String.valueOf(produto.getFotoPrincipal());
+       ImageView imagem1 = (ImageView)findViewById(R.id.imageViewDetalhesFoto);
+       imagem1.setImageBitmap(DownloadImage(URL1));
+
+        this.fotoPrincipal.setImageBitmap(DownloadImage(URL1));
 
     }
+
+
+
+    /*
+     *
+     * @URL
+     * Ler uma url de imagem
+     *
+     * */
+    @SuppressLint("NewApi")
+    public Bitmap DownloadImage(String pURL){
+
+        StrictMode.ThreadPolicy vPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(vPolicy);
+
+        InputStream inStream = null;
+        Bitmap vBitmap = null;
+
+        try{
+
+            URL url = new URL(pURL);
+            HttpURLConnection pConnection = (HttpURLConnection)url.openConnection();
+            pConnection.setDoInput(true);
+            pConnection.connect();
+
+            if(pConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
+
+                inStream = pConnection.getInputStream();
+                vBitmap = BitmapFactory.decodeStream(inStream);
+                inStream.close();
+
+                return vBitmap;
+            }
+
+        }
+        catch(Exception ex){
+            Log.e("Exception",ex.toString());
+        }
+
+        return null;
+
+    }
+
 }
